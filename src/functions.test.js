dc0961900c24372ed4de7495a01e72b3dd9a50a1
Juan Pablo Sánchez Rey <juanpablosanchez744@gmail.com>
2021-12-08 17:05:11 -0500

    se completaron todos los test

--- src/functions.test.js
+++ src/functions.test.js
@@ -1,6 +1,6 @@
 import { createEvent } from "./functions";
 beforeAll(() => {
-  global.Date.now = jest.fn(() => new Date("2020-12-08T10:20:30Z").getTime());
+  global.Date.now = jest.fn(() => new Date("2021-12-07T10:20:30Z").getTime());
 });
 
 test("Validation a event title and content basic", () => {
@@ -8,33 +8,66 @@ test("Validation a event title and content basic", () => {
 
   expect(result.title).toBe("[SOFKA U] Meeting Room");
   expect(result.description).toBe("Mentoring and Practice");
-  expect(result.duration).toEqual([6, "hour"]);
+  expect(result.duration).toEqual([10, "hour"]);
 });
 
 test("Validation start date", () => {
   const numDayPrueba = NUM_DAY[weekday];
   const currentDayPrueba = new Date().getDay();
+
   const iniciarTest = getDateCalendar(numDayPrueba, currentDayPrueba);
   const resultado = createEvent(weekday, week, openHour, closeHour);
   expect(resultado.start).toStrictEqual(iniciarTest);
 });
 
 test("Validation date", () => {
-  //TODO: hacer las verificaciones
+  const tiempo = Date.now();
+  const date = new Date(tiempo);
+  const fechaTest = new Date(date).toLocaleString("es-ES", options);
+  let resultado = createEvent(weekday, week, openHour, closeHour);
+  expect(resultado.date).toBe(fechaTest);
 });
 
 test("Validation illegal arguments", () => {
-  //TODO: hacer las verificaciones
+  const weekday = "alv";
+  const week = -2;
+  const openHour = -6;
+  const closeHour = -4;
+  if (
+    closeHour - openHour < 0 ||
+    week < 0 ||
+    !Object.keys(NUM_DAY).some((key) => key === weekday)
+  ) {
+    const error = () => {
+      createEvent(weekday, week, openHour, closeHour);
+    };
+    expect(error).toThrow(Error);
+  }
 });
 
+const title = "[SOFKA U] Meeting Room";
+const description = "Mentoring and Practice";
 test("create an event list of at least 10 events", () => {
-  //TODO: hacer las verificaciones
+  listaEventos.map((evento) => {
+    const resultado = createEvent(
+      evento.weekday,
+      evento.week,
+      evento.openHour,
+      evento.closeHour
+    );
+    expect(resultado.title).toBe(title);
+    expect(resultado.description).toBe(description);
+    expect(resultado.duration).toEqual([
+      evento.closeHour - evento.openHour,
+      "hour",
+    ]);
+  });
 });
 
-const weekday = "mon";
-const week = 2;
+const weekday = "tue";
+const week = 1;
 const openHour = 8;
-const closeHour = 14;
+const closeHour = 18;
 
 const NUM_DAY = { mon: 1, tue: 2, wed: 3, thu: 4, fri: 5, sat: 6, sun: 7 };
 
@@ -58,3 +91,54 @@ function getDateCalendar(numDay, currentDay) {
   }
   return addDays(numDay - currentDay + 7 * (week - 1));
 }
+
+const listaEventos = [
+  {
+    weekday: "mon",
+    week: 2,
+    openHour: 10,
+    closeHour: 12,
+  },
+  {
+    weekday: "tue",
+    week: 2,
+    openHour: 10,
+    closeHour: 12,
+  },
+  {
+    weekday: "sat",
+    week: 5,
+    openHour: 12,
+    closeHour: 16,
+  },
+  {
+    weekday: "thu",
+    week: 1,
+    openHour: 10,
+    closeHour: 11,
+  },
+  {
+    weekday: "sun",
+    week: 3,
+    openHour: 5,
+    closeHour: 7,
+  },
+  {
+    weekday: "fri",
+    week: 7,
+    openHour: 8,
+    closeHour: 10,
+  },
+  {
+    weekday: "sun",
+    week: 1,
+    openHour: 7,
+    closeHour: 11,
+  },
+  {
+    weekday: "tue",
+    week: 12,
+    openHour: 7,
+    closeHour: 14,
+  },
+];
